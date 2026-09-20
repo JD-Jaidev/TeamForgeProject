f6b71b1d3448ab60ad8c33cb5472a1f90e823aa8
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-09-20 20:17:40 +0530

    feat : program files added

--- TeamForgeFiles/js/ui.js
+++ TeamForgeFiles/js/ui.js
@@ -257,7 +257,18 @@ class TeamForgeUI {
     }
   }
 
+  static ensureFavicon() {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.head.appendChild(link);
+    }
+    link.href = 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>';
+  }
+
   static initPage(activePage = '') {
+    this.ensureFavicon();
     this.renderNavbar(activePage);
     this.renderSidebar(activePage);
     this.renderFooter();
